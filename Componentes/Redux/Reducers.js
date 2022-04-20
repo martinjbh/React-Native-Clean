87a73198203136c0d7208db8f3b48c4d6710c984
martin <barreiromartinj@gmail.com>
2022-04-20 01:26:16 -0300

    first commit

--- Componentes/Redux/Reducers.js
+++ Componentes/Redux/Reducers.js
@@ -1,11 +1,12 @@
-
+var id = 1
 const initialState = {
-    movies: [{"name":"volver al futuro","año":1988},{"name":"star wars","año":1998}],
-   
+
+    movies: [{ "name": "volver al futuro", "año": 1988 }],
+
 }
 export const reducer = (state = initialState, action) => {
     switch (action.type) {
-  
+
         default:
             return state
     }
--- Componentes/ShowRoom/ShowRoom.js
+++ Componentes/ShowRoom/ShowRoom.js
@@ -1,22 +1,44 @@
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, SafeAreaView, FlatList, SectionList } from 'react-native';
 import { useSelector } from 'react-redux';
 import { useDispatch } from 'react-redux';
 export default function ShowRoom({ navigation }) {
     const dispatch = useDispatch()
     const state = useSelector(state => state.movies)
+
+    const enviar = () => {
+        navigation.navigate('Home')
+    }
     return (
         <View style={styles.container}>
-            {state.map((e, i) => {
-                return <Text key={i}>{e.name}</Text>
-            })}
 
+            {/* {state.map((e, i) => {
+                return <Text key={i}>{e.name}</Text>
+            })} */}
             <TouchableOpacity
                 style={styles.containerBoton}
-                onPress={() => navigation.navigate('Home')}>
+                onPress={enviar}>
                 <Text >ir a Home</Text>
             </TouchableOpacity>
+            <SafeAreaView style={styles.nombres}>
+                <FlatList
+                    data={state}
+                    keyExtractor={(item, index) => index}
+                    renderItem={({ item }) =>
+                        2 == 2 ? <Text >{item.name}</Text> : console.log("no muestra nada")
+                    }
+                />
+            </SafeAreaView>
+            {/* <SafeAreaView style={styles.nombres2}>
+                <SectionList
+                    sections={state}
+                    keyExtractor={(item, index) => item + index}
+                    renderItem={({ item }) =>  <Text key={item.id}>{item.name}</Text>}
+                    renderSectionHeader={({ section: { title } }) => (
+                        <Text style={styles.header}>{title}</Text>
+                    )}
+                />
+            </SafeAreaView> */}
         </View>
-
     );
 }
 const styles = StyleSheet.create({
@@ -24,10 +46,27 @@ const styles = StyleSheet.create({
         flex: 1,
         alignItems: 'center',
         justifyContent: 'center',
+        top: 100
     },
     containerBoton: {
         padding: '1%',
         backgroundColor: "green",
         borderRadius: 9
     },
+    nombres: {
+        height: 100,
+        width: 150,
+        marginTop: 100,
+        backgroundColor: "red",
+        padding: "1%",
+        borderRadius: 12
+    },
+    nombres2: {
+        height: 100,
+        width: 150,
+        marginTop: 200,
+        backgroundColor: "red",
+        padding: "1%",
+        borderRadius: 12
+    }
 });
